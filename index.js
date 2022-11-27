e1cf27abd388fe8a6d481310964d336db2319495
trung_dev_be <trungdh@tradeline.vn>
2022-11-27 13:47:40 +0700

    init canvas ui

---
+++ index.js
@@ -0,0 +1,16 @@
+﻿const path = require('path')
+const express = require('express')
+const mg = require('morgan')
+const app = express()
+const port = 8901
+
+app.use(
+	mg(
+		'{\n user_IP: :remote-addr \n remote_user: :remote-user \n time: :date[clf] \n method: :method \n url: :url \n httpversion: :http-version \n status: :status\n res: :res[content-length] \n ref: :referrer \n user agent: :user-agent \n response time: :response-time[digits] ms \n total time: :total-time[digits] ms\n}'
+	)
+)
+app.use(express.static('src/public'))
+
+app.listen(port, () => {
+	console.log(`App listening on port ${port}`)
+})
---
+++ public/CanvasUI/panel/app.js
@@ -0,0 +1,100 @@
+// see devsrv.config.json for threejs version dynamically replaced by devsrv
+import * as THREE from 'https://cdn.skypack.dev/three@0.135';
+import { BoxLineGeometry } from 'https://cdn.skypack.dev/three@0.135/examples/jsm/geometries/BoxLineGeometry.js';
+
+import { CanvasUI } from '../../jsm/CanvasUI.js'
+import { VRButton } from '../../jsm/VRButton.js';
+
+class App{
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
+                
+		this.camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 100 );
+		this.camera.position.set( 0, 1.6, 0 );
+        
+		this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color( 0x505050 );
+        this.scene.add( this.camera );
+
+		this.scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
+
+        const light = new THREE.DirectionalLight( 0xffffff );
+        light.position.set( 1, 1, 1 ).normalize();
+		this.scene.add( light );
+			
+		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+        this.renderer.outputEncoding = THREE.sRGBEncoding;
+        
+		container.appendChild( this.renderer.domElement );
+        
+        this.initScene();
+        this.setupXR();
+        
+        window.addEventListener('resize', this.resize.bind(this) );
+	}	
+    
+    initScene(){
+        this.room = new THREE.LineSegments(
+					new BoxLineGeometry( 6, 6, 6, 10, 10, 10 ),
+					new THREE.LineBasicMaterial( { color: 0x808080 } )
+				);
+        this.room.geometry.translate( 0, 3, 0 );
+        this.scene.add( this.room );
+        
+        this.createUI();
+    }
+    
+    createUI() {
+        const css = {
+            header:{
+                type: "text",
+                position:{ top:0 },
+                paddingTop: 30,
+                height: 70
+            },
+            main:{
+                type: "text",
+                position:{ top:70 },
+                height: 372, // default height is 512 so this is 512 - header height:70 - footer height:70
+                backgroundColor: "#bbb",
+                fontColor: "#000"
+            },
+            footer:{
+                type: "text",
+                position:{ bottom:0 },
+                paddingTop: 30,
+                height: 70
+            }
+        }
+        const content = {
+            header: "Header",
+            main: "This is the main text",
+            footer: "Footer"
+        }
+        this.ui = new CanvasUI( content, css );
+        this.ui.mesh.position.set( 0, 1.5, -1.6 );
+        this.camera.attach( this.ui.mesh );
+    }
+    
+    setupXR(){
+        this.renderer.xr.enabled = true; 
+        new VRButton( this.renderer );
+        this.renderer.setAnimationLoop( this.render.bind(this) );
+    }
+    
+    resize(){
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );  
+    }
+    
+	render( ) {   
+        if ( this.renderer.xr.isPresenting ) this.ui.update();
+        this.renderer.render( this.scene, this.camera );
+    }
+}
+
+export { App };
